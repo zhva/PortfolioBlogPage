ff7f7bc2a6bae24e4ae7ad07c52dcd7df6a6fae9
Viktoriia <zhuravel.victoriia@gmail.com>
2019-11-01 15:48:39 +0100

    Init personal blog webpage repository

---
+++ js/gallery.js
@@ -0,0 +1,13 @@
+function fadeInOutGallery(containerId, galleryItemsClass) {
+  let current = 0;
+  const slidesContainer = document.getElementById(containerId);
+  const slides = slidesContainer.getElementsByClassName(galleryItemsClass);
+
+  setInterval(() => {
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.opacity = 0;
+    }
+    current = (current !== slides.length - 1) ? current + 1 : 0;
+    slides[current].style.opacity = 1;
+  }, 5000);
+}
---
+++ js/sideMenu.js
@@ -0,0 +1,7 @@
+function openMenu(menuId) {
+  document.getElementById(menuId).style.width = '30%';
+}
+
+function closeMenu(menuId) {
+  document.getElementById(menuId).style.width = '0';
+}
