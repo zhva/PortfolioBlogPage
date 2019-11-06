fd9a700160249626c8c9e87b756685b742729c0d
Viktoriia <zhuravel.victoriia@gmail.com>
2019-11-06 14:41:33 +0100

    sideMenu update

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -1,9 +1,37 @@
-function openMenu(menuId) {
-  document.getElementById(menuId).style.width = '25%';
+function openMenu(menuId, menuOpen, menuClose) {
+  const objMenu = document.getElementById(menuId);
+  if (objMenu == null) {
+    return;
+  }
+
+  objMenu.classList.remove(menuClose);
+  objMenu.classList.add(menuOpen);
   document.getElementsByTagName('body')[0].style.overflow = 'hidden';
 }
 
-function closeMenu(menuId) {
-  document.getElementById(menuId).style.width = '0';
+function closeMenu(menuId, menuOpen, menuClose) {
+  const objMenu = document.getElementById(menuId);
+  if (objMenu == null) {
+    return;
+  }
+
+  objMenu.classList.remove(menuOpen);
+  objMenu.classList.add(menuClose);
   document.getElementsByTagName('body')[0].style.overflow = 'auto';
+}
+
+function toggleMenuState(elementId, subMenuClose) {
+  const menuElement = document.getElementById(elementId);
+  if (menuElement == null) {
+    return;
+  }
+  const subMenuOgj = menuElement.nextElementSibling;
+  if (subMenuOgj.classList.contains(subMenuClose)) {
+    menuElement.classList.replace('triangle-down', 'triangle-up');
+    subMenuOgj.classList.remove(subMenuClose);
+  }
+  else {
+    menuElement.classList.replace('triangle-up', 'triangle-down');
+    subMenuOgj.classList.add(subMenuClose);
+  }
 }
